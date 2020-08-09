6e2bda80bf189be9624299a7d5b6ce14b89c6978
MyeongchanJin <jmcchan@naver.com>
2020-08-09 23:55:26 +0900

    Initial commit

---
+++ src/main/java/com/npee/myproject/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.npee.myproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @ResponseBody
+    @GetMapping("/")
+    public String home() {
+        return "home test";
+    }
+}
