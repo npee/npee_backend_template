31fdbebbd648acb6c230c0e7875a4fb6b7aec632
MyeongchanJin <jmcchan@naver.com>
2020-08-24 15:28:41 +0900

    JPA Interface 생성

---
+++ src/main/java/com/npee/myproject/repository/UserJpaRepository.java
@@ -0,0 +1,12 @@
+package com.npee.myproject.repository;
+
+import com.npee.myproject.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * userJpaRepository
+ * JpaRepository를 상속받은 인터페이스 생성
+ * 제네릭으로 객체 타입(Entity)과 Long을 사용한다.
+ * */
+public interface UserJpaRepository extends JpaRepository<User, Long> {
+}
