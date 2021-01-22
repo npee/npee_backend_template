d0c6794888e019a88a7f9fcc4fd2ef40714bbe8f
npee <jmc4664@gmail.com>
2021-01-22 19:52:20 +0900

    API 부분을 API 컨트롤러로 분리

---
+++ src/main/java/com/npee/myproject/api/UserApiController.java
@@ -0,0 +1,53 @@
+package com.npee.myproject.api;
+
+import com.npee.myproject.config.response.ListResult;
+import com.npee.myproject.config.response.ResponseService;
+import com.npee.myproject.config.response.SingleResult;
+import com.npee.myproject.domain.entity.dto.RequestUserDto;
+import com.npee.myproject.domain.entity.dto.ResponseUserDto;
+import com.npee.myproject.service.UserServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Api(tags = {"1. User"})
+@RestController
+// @CrossOrigin(origins = {"http://127.0.0.1:3000"}) // TODO: 다른 포트와 통신해야 할 경우 CORS 이슈 체크
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/api")
+public class UserApiController {
+
+    private final ResponseService responseService;
+    private final UserServiceImpl userService;
+
+    @ApiOperation(value = "회원 목록", notes = "회원 목록 불러오기")
+    @GetMapping("/v1/all-users")
+    // TODO: Entity가 준비되면 CommonResult에서 상황에 맞게 SingleResult나 ListResult로 변경
+    // TODO: Entity에 직접 접근하지 않기 위해 DTO 이용
+    public ListResult<ResponseUserDto> getUsers() {
+        return responseService.getListResult(userService.findAllUsers());
+    }
+
+    @ApiOperation(value = "회원 검색", notes = "특정 회원 검색")
+    @GetMapping("/v1/users")
+    public SingleResult<ResponseUserDto> getUser(@RequestParam String id,
+                                                 @RequestParam String pw) {
+        return null;
+    }
+
+    @ApiOperation(value = "회원 추가/수정", notes = "회원 업데이트")
+    @PostMapping("/v1/users")
+    public SingleResult<ResponseUserDto> setUser(@RequestBody RequestUserDto userDto) {
+        return null;
+    }
+
+    @ApiOperation(value = "회원 삭제", notes = "회원 삭제")
+    @DeleteMapping("/v1/users/{userNo}")
+    public SingleResult<ResponseUserDto> deleteUser(@PathVariable Long userNo) {
+        return null;
+    }
+
+}
